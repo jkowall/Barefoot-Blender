6a40dff27c2caeceb78c63eba63a69a46d07e270
Jonah Kowall <jkowall@kowall.net>
2026-05-25 14:33:04 -0400

    Standardize tests on Vitest (#122)

--- src/state/settings.test.ts
+++ src/state/settings.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from "bun:test";
+import { describe, expect, test } from "vitest";
 import { GAS_NAME_MAX_LENGTH } from "../utils/gasNames";
 import { migrateSettingsState, sanitizeCustomGas } from "./settings";
 
